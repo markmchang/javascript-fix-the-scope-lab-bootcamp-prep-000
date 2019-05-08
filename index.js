56b2b402057636ee22a4b8b15266e843cae8dcfe
Mark Chang <chang.m.mark@gmail.com>
2019-05-08 16:14:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  const two = 2
-  n = '215'
   return n + two
 
   // Feel free to move things around!
+  const two = 2
 }
 
 var funkyFunction = function() {
