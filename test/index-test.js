0e6e041c86de37e771d4431881f248b5e5a0ebdb
Mark Chang <chang.m.mark@gmail.com>
2019-05-08 16:39:28 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -26,7 +26,7 @@ describe('Fix the Scope', function() {
 
   describe('funkyFunction()', function() {
     it('returns a function', function() {
-      expect(window.funkyFunction.toString()).toEqual('function')
+      expect(typeof window.funkyFunction()).toEqual('function')
     })
   })
 
